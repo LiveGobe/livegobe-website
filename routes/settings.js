fb4bf741a2cf5ff0dc9b23e2ce2828acff746b7e
LiveGobe <pax01012000@gmail.com>
2024-03-18 16:27:22 +1000

    Update settings router

--- routes/settings.js
+++ routes/settings.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
+    if (!req.user) return res.redirect("/");
+
     res.serve("settings");
 });
 
