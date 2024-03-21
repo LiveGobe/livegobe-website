cbafa6af67b8f68373dfdf4818fc420ccc0de9a5
LiveGobe <pax01012000@gmail.com>
2024-03-21 13:28:20 +1000

    Create register key generation script

---
+++ misc/createRegistrationKey.js
@@ -0,0 +1,36 @@
+const prompt = require("prompt");
+const colors = require("@colors/colors/safe");
+const RegisterKey = require("../models/registerKey");
+const mongoose = require("mongoose");
+const config = require("../config");
+const bcrypt = require("bcrypt");
+
+prompt.message = "";
+prompt.delimiter = "";
+
+prompt.start();
+
+const prompts = {
+    env: {
+        name: "env",
+        description: colors.yellow("Environment (dev/prod):"),
+        type: "string",
+        required: true
+    }
+}
+
+// Ask for environment
+prompt.get(prompts.env, async function(err, env) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    env = env.env == "production" || env.env == "prod" || env.env == "p" ? "production" : "development"
+    await mongoose.connect(env == "production" ? config.mongodb.uriProd : config.mongodb.uriDev)
+    const registerKey = new RegisterKey();
+    await registerKey.save();
+    console.log(colors.yellow(`Registration key: `) + colors.green(registerKey.key));
+    console.log(colors.yellow(`Registration link: `) + colors.green(`${"https://" + env == "production" ? config.domainName : `localhost:${config.port}` + "/register?key=" + new URLSearchParams(registerKey.key)}`));
+    mongoose.connection.close();
+});
