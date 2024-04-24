50613c7ba25a3044853a6155267a7f5ef7884ae6
LiveGobe <pax01012000@gmail.com>
2024-04-24 17:51:00 +1000

    Add Bundle component
    Bundle component takes a name of the bundle and creates a corresponding tag (script/link) with auto versioning

---
+++ src/components/bundle.jsx
@@ -0,0 +1,16 @@
+const React = require('react');
+const pck = require("../../package.json");
+const utils = require("../../bin/utils");
+
+function Bundle(props) {
+    if (props.name.endsWith("js")) return (
+        <script src={utils.bundleUrl(props.name.replace(".", `-${pck.version}.`))} />
+    );
+    else if (props.name.endsWith("css")) return (
+        <link rel="stylesheet" href={utils.bundleUrl(props.name.replace(".", `-${pck.version}.`))} />
+    );
+    
+    return "";
+}
+
+module.exports = Bundle;
